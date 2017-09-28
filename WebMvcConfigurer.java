eb96e3cda2b8296091195f283401d215cd387a23
2356899074@qq.com <2356899074@qq.com>
2017-09-28 11:20:41 +0800

    springmvc 跨域

---
+++ WebMvcConfigurer.java
@@ -0,0 +1,20 @@
+package com.gstar.acp.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowCredentials(true)
+				.allowedMethods("*")
+				.maxAge(3600);
+	}
+
+}
